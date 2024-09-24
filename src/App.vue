df2e3fac3861a3bc54fbbc5ac362280a38c8fcc8
Abhi-CodesCode <abhi.codescode@gmail.com>
2024-09-24 23:37:26 +0530

    Services Section and About Section added

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-[#111827] min-h-screen">
+    <NavBar/>
+    <HeroSection/>
+    <ServicesSection/>
+    <AboutSection/>
+  </div>
+</template>
+
+<script setup> 
+import { defineAsyncComponent } from 'vue';
+//Import Components Asynchronously 
+const NavBar = defineAsyncComponent(()=>import('@/components/NavBar.vue'));
+const HeroSection = defineAsyncComponent(()=>import('@/components/HeroSection.vue'));
+const ServicesSection = defineAsyncComponent(()=>import('@/components/ServicesSection.vue'));
+const AboutSection = defineAsyncComponent(()=>import('@/components/AboutSection.vue'));
+
+</script>
